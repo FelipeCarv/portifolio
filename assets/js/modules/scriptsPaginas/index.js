1d0cd13364521329166ad64d17e7c621ad9f173d
felipecarvalho <felipe.carvalho_2016@outlook.com>
2022-08-28 15:02:29 -0300

    adicionando alguns efeitos ao portifólio, falta o responsivo

--- assets/js/modules/scriptsPaginas/index.js
+++ assets/js/modules/scriptsPaginas/index.js
@@ -1,5 +1,17 @@
 export default function scriptsIndex(){
     // 'colocar scripts das respectivas paginas dentro de -scriptsPaginas-  e criar o arquivo exemplo: home.js - contato.js  --- e importar no main e executar em sua respectiva pagina '
-
+    function typeWrite(elemento){
+        const textoArray = elemento.innerHTML.split('');
+        elemento.innerHTML = ' ';
+        textoArray.forEach(function(letra, i){   
+          
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 75 * i)
+    
+      });
+    }
+    const titulo = document.querySelector('.titulo-principal');
+    typeWrite(titulo);
     
 }
